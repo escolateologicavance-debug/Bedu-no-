0f61587716a6623e3a47cee54e7302dba1013a50
Gilberto Moreira <escolateologicavance@gmail.com>
2026-01-02 15:44:19 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'beduino-v1';
+const assets = [
+  './',
+  './index.html',
+  './quiz1.html',
+  './quiz2.html',
+  './quiz3.html',
+  './quiz4.html',
+  './quiz5.html',
+  './quiz6.html',
+  './quiz7.html',
+  './quiz8.html',
+  './quiz9.html',
+  './quiz10.html',
+  './assets/logo-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
